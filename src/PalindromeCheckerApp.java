4bb2474f623a946fe50c21f2ef0cc608afe326c8
2006veda <movvaveda8@gmail.com>
2026-02-23 11:16:14 +0530

    use case 7

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,27 +1,27 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
-public class UseCase6PalindromeCheckerApp{
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class UseCase7PalindromeCheckerApp{
     public static void main(String[] args){
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
         for (char c : input.toCharArray()) {
-            queue.add(c);     // Enqueue (FIFO)
-            stack.push(c);    // Push (LIFO)
+            deque.addLast(c);
         }
         boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            char fromQueue = queue.remove(); // Dequeue
-            char fromStack = stack.pop();    // Pop
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last  = deque.removeLast();
 
-            if (fromQueue != fromStack) {
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
         }
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+
+
 
 
     }
