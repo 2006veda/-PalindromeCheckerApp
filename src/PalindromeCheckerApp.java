58817617ceb21b4309b69c7a95baeaf0eb25f8a4
2006veda <movvaveda8@gmail.com>
2026-03-02 10:08:44 +0530

    Use Case 11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,41 @@
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
 
-        String input = "A man a plan a canal Panama";
+    String input = "madam";
+
+    PalindromeService service = new PalindromeService();
+
+    boolean result = service.checkPalindrome(input);
+
+    System.out.println("Input : " + input);
+    System.out.println("Is Palindrome? : " + result);
+}
+}
 
 
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        boolean isPalindrome = true;
+class PalindromeService {
 
 
-        for (int i = 0; i < normalized.length() / 2; i++) {
+    public boolean checkPalindrome(String input) {
 
-
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        if (input == null) {
+            return false;
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        int start = 0;
+        int end = input.length() - 1;
+
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
